a201f27ad9cb3b2314958cb1a9a5701e6811f188
Samuel Torres <samuelvianatorres@gmail.com>
2025-07-28 16:55:01 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioProposto6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+namespace MyApp
+{
+    internal class ExercicioProposto6
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            double a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            double triangulo = (a * c) / 2.0;
+            double circulo = 3.14159 * c * c;
+            double trapezio = ((a + b) * c) / 2.0;
+            double quadrado = b * b;
+            double retangulo = a * b;
+
+            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+
+
+
+
+
+
+        }
+    }
+}
